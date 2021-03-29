da6e10d0776b78ccbb10a493a2885ec6f2e3dcd6
DESKTOP-MEONOPQ\slopez <synlo@outlook.com>
2021-03-29 21:31:20 +0200

    Séparation fichier

--- src/main.go
+++ src/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"container/heap"
 	"fmt"
-	"io/ioutil"
 	"math"
-	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -101,67 +96,6 @@ func GenerateHistory(node Node) {
 	fmt.Print("\n")
 }
 
-func ParseContent(content string) (int, []int) {
-	var size		int
-	var result		[]int
-
-	size				= -1
-	content_splitted 	:= strings.Split(content, "\n")
-
-	for line_number, line := range content_splitted {
-		if len(line) < 1 || line[0] == '#' {
-			continue
-		}
-		if size == -1 {
-			atoi, err := strconv.Atoi(line)
-			if err != nil {
-				fmt.Println("Error puzzle size, line", line_number + 1)
-				os.Exit(0)
-			}
-			size = atoi
-		} else {
-			split_values := strings.Split(line, " ")
-			for _, nb := range split_values {
-				tmp := strings.TrimSpace(nb)
-				if len(tmp) > 0 {
-					atoi, err := strconv.Atoi(tmp)
-					if err != nil {
-						fmt.Println("Error puzzle, line", line_number + 1)
-						os.Exit(0)
-					}
-					result = append(result, atoi)
-				}
-			}
-		}
-	}
-	return size, result
-}
-
-func Parse() (int, []int) {
-	args := os.Args[1:]
-
-	if len(args) > 0 {
-		if (args[0] == "-f") {
-			if len(args) > 1 {
-				data, err := ioutil.ReadFile(args[1])
-				if err != nil {
-					fmt.Println("Error reading file", args[1])
-					os.Exit(1)
-				}
-				return ParseContent(string(data))
-			} else {
-				fmt.Println("You must specify a file")
-				os.Exit(1)
-			}
-		}
-	} else {
-		reader 		:= bufio.NewReader(os.Stdin)
-		content, _ 	:= reader.ReadString(0)
-		return ParseContent(content)
-	}
-	return -1, nil
-}
-
 func main() {
 	var common				Common
 	var base 				[]int
@@ -188,7 +122,7 @@ func main() {
 	heap.Push(&common.open_set, &Item{node: node, priority: 0})
 
 	max_iterations := 0
-	for max_iterations < 375000 {
+	for {
 
 		if common.open_set.Len() == 0 {
 			fmt.Println("\033[1;31mEmpty queue, break.\033[0m")
